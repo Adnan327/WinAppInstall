3e2d0bd62465f9e48407301f053ee9305814cbe6
Adnan327 <adnan.adem.am@gmail.com>
2024-08-19 23:46:13 +0200

    Added colored font

--- WinAppInstall.ps1
+++ WinAppInstall.ps1
@@ -13,26 +13,38 @@ if ($input1 -eq 'Y' -or $input1 -eq 'y') {
     Start-Sleep -Seconds 0.5
 
     # install Google Chrome
-    $chrome = Read-Host "Möchtest du Google Chrome installieren? (Y/N)"
+    Write-Host "Möchtest du " -NoNewline
+    Write-Host "'Google Chrome' " -ForegroundColor Yellow -NoNewline
+    Write-Host "installieren? (Y/N)" -NoNewline
+    $chrome = Read-Host
     if ($chrome -eq 'Y' -or $chrome -eq 'y') {
-        Write-Host "Google Chrome wird installiert..."
-        winget install Google.Chrome -h --accept-package-agreements --accept-source-agreements
+        Write-Host "'Google Chrome' " -ForegroundColor Yellow -NoNewline
+        Write-Host "wird installiert..." 
+        #winget install Google.Chrome -h --accept-package-agreements --accept-source-agreements
         Write-Host ""
     }
 
     # install Firefox
-    $firefox = Read-Host "Möchtest du Firefox installieren? (Y/N)"
+    Write-Host "Möchtest du " -NoNewline
+    Write-Host "'Firefox' " -ForegroundColor Yellow -NoNewline
+    Write-Host "installieren? (Y/N)" -NoNewline
+    $firefox = Read-Host
     if ($firefox -eq 'Y' -or $firefox -eq 'y') {
-        Write-Host "Fireox wird installiert..."
-        winget install Mozilla.Firefox -h --accept-package-agreements --accept-source-agreements
+        Write-Host "'Firefox' " -ForegroundColor Yellow -NoNewline
+        Write-Host "wird installiert..." 
+        #winget install Mozilla.Firefox -h --accept-package-agreements --accept-source-agreements
         Write-Host ""
     }
 
     # install Brave
-    $brave = Read-Host "Möchtest du Brave installieren? (Y/N)"
+    Write-Host "Möchtest du " -NoNewline
+    Write-Host "'Brave' " -ForegroundColor Yellow -NoNewline
+    Write-Host "installieren? (Y/N)" -NoNewline
+    $brave = Read-Host 
     if ($brave -eq 'Y' -or $brave -eq 'y') {
-        Write-Host "Brave wird installiert..."
-        winget install Brave.Brave -h --accept-package-agreements --accept-source-agreements
+        Write-Host "'Brave' " -ForegroundColor Yellow -NoNewline
+        Write-Host "wird installiert..."
+        #winget install Brave.Brave -h --accept-package-agreements --accept-source-agreements
         Write-Host ""
     } else {
         Write-Host ""
@@ -49,50 +61,74 @@ if ($input1 -eq 'Y' -or $input1 -eq 'y') {
     Write-Host ""
     Start-Sleep -Seconds 0.5
 
-    # install VSCode
-    $vscode = Read-Host "Möchtest du VSCode installieren? (Y/N)"
+    # install Visual Studio Code
+    Write-Host "Möchtest du " -NoNewline
+    Write-Host "'Visual Studio Code' " -ForegroundColor Yellow -NoNewline
+    Write-Host "installieren? (Y/N)" -NoNewline
+    $vscode = Read-Host 
     if ($vscode -eq 'Y' -or $vscode -eq 'y') {
-        Write-Host "VSCode wird installiert..."
-        winget install Microsoft.VisualStudioCode -h --accept-package-agreements --accept-source-agreements
+        Write-Host "'Visual Studio Code' " -ForegroundColor Yellow -NoNewline
+        Write-Host "wird installiert..."
+        #winget install Microsoft.VisualStudioCode -h --accept-package-agreements --accept-source-agreements
         Write-Host ""
     }
 
     # install Git
-    $git = Read-Host "Möchtest du Git installieren? (Y/N)"
+    Write-Host "Möchtest du " -NoNewline
+    Write-Host "'Git' " -ForegroundColor Yellow -NoNewline
+    Write-Host "installieren? (Y/N)" -NoNewline
+    $git = Read-Host
     if ($git -eq 'Y' -or $git -eq 'y') {   
-        Write-Host "Git wird installiert..."
-        winget install Git.Git -h --accept-package-agreements --accept-source-agreements
+        Write-Host "'Git' " -ForegroundColor Yellow -NoNewline
+        Write-Host "wird installiert..."
+        #winget install Git.Git -h --accept-package-agreements --accept-source-agreements
         Write-Host ""
     }
 
     # install Oracle.jdk.22
-    $jdk22 = Read-Host "Möchtest du Oracle.jdk.22 installieren? (Y/N)"
+    Write-Host "Möchtest du " -NoNewline
+    Write-Host "'Oracle.jdk.22' " -ForegroundColor Yellow -NoNewline
+    Write-Host "installieren? (Y/N)" -NoNewline
+    $jdk22 = Read-Host 
     if ($jdk22 -eq 'Y' -or $jdk22 -eq 'y') { 
-        Write-Host "Oracle.jdk.22 wird installiert..."
-        winget install Oracle.JDK.22 -h --accept-package-agreements --accept-source-agreements
+        Write-Host "'Oracle.jdk.22' " -ForegroundColor Yellow -NoNewline
+        Write-Host "wird installiert..."
+        #winget install Oracle.JDK.22 -h --accept-package-agreements --accept-source-agreements
         Write-Host ""
     }
     # install Powershell
-    $powershell = Read-Host "Möchtest du Powershell installieren? (Y/N)"
+    Write-Host "Möchtest du " -NoNewline
+    Write-Host "'Powershell' " -ForegroundColor Yellow -NoNewline
+    Write-Host "installieren? (Y/N)" -NoNewline
+    $powershell = Read-Host
     if ($powershell -eq 'Y' -or $powershell -eq 'y') { 
-        Write-Host "Powershell wird installiert..."
-        winget install Microsoft.PowerShell -h --accept-package-agreements --accept-source-agreements
+        Write-Host "'Powershell' " -ForegroundColor Yellow -NoNewline
+        Write-Host "wird installiert..."
+        #winget install Microsoft.PowerShell -h --accept-package-agreements --accept-source-agreements
         Write-Host ""
     }
 
-    # install Windows-Terminal
-    $winterminal = Read-Host "Möchtest du Windows-Terminal installieren? (Y/N)"
+    # install Windows Terminal
+    Write-Host "Möchtest du " -NoNewline
+    Write-Host "'Windows Terminal' " -ForegroundColor Yellow -NoNewline
+    Write-Host "installieren? (Y/N)" -NoNewline
+    $winterminal = Read-Host 
     if ($winterminal -eq 'Y' -or $winterminal -eq 'y') { 
-        Write-Host "Windows-Terminal wird installiert..."
-        winget install Microsoft.WindowsTerminal -h --accept-package-agreements --accept-source-agreements
+        Write-Host "'Windows Terminal' " -ForegroundColor Yellow -NoNewline
+        Write-Host "wird installiert..."
+        #winget install Microsoft.WindowsTerminal -h --accept-package-agreements --accept-source-agreements
         Write-Host ""
     }
 
     # install VirtualBox
-    $vb = Read-Host "Möchtest du VirtualBox installieren? (Y/N)"
+    Write-Host "Möchtest du " -NoNewline
+    Write-Host "'VirtualBox' " -ForegroundColor Yellow -NoNewline
+    Write-Host "installieren? (Y/N)" -NoNewline
+    $vb = Read-Host 
     if ($vb -eq 'Y' -or $vb -eq 'y') { 
-        Write-Host "VirtualBox wird installiert..."
-        winget install VirtualBox Oracle.VirtualBox 
+        Write-Host "'VirtualBox' " -ForegroundColor Yellow -NoNewline
+        Write-Host "wird installiert..."
+        #winget install VirtualBox Oracle.VirtualBox 
         Write-Host ""
     } else {
         Write-Host ""
@@ -110,42 +146,62 @@ if ($input1 -eq 'Y' -or $input1 -eq 'y') {
     Start-Sleep -Seconds 0.5
 
     # install Office365
-    $office365 = Read-Host "Möchtest du Office365 installieren? (Y/N)"
+    Write-Host "Möchtest du " -NoNewline
+    Write-Host "'Office365' " -ForegroundColor Yellow -NoNewline
+    Write-Host "installieren? (Y/N)" -NoNewline
+    $office365 = Read-Host
     if ($office365 -eq 'Y' -or $office365 -eq 'y') { 
-        Write-Host "Office365 wird installiert..."
-        winget install Microsoft.Office -h --accept-package-agreements --accept-source-agreements
+        Write-Host "'Office365' " -ForegroundColor Yellow -NoNewline
+        Write-Host "wird installiert..."
+        #winget install Microsoft.Office -h --accept-package-agreements --accept-source-agreements
         Write-Host ""
     }
 
     # install LibreOffice
-    $libre = Read-Host "Möchtest du LibreOffice installieren? (Y/N)"
+    Write-Host "Möchtest du " -NoNewline
+    Write-Host "'LibreOffice' " -ForegroundColor Yellow -NoNewline
+    Write-Host "installieren? (Y/N)" -NoNewline
+    $libre = Read-Host
     if ($libre -eq 'Y' -or $libre -eq 'y') { 
-        Write-Host "LibreOffice wird installiert..."
-        winget install TheDocumentFoundation.LibreOffice -h --accept-package-agreements --accept-source-agreements
+        Write-Host "'LibreOffice' " -ForegroundColor Yellow -NoNewline
+        Write-Host "wird installiert..."
+        #winget install TheDocumentFoundation.LibreOffice -h --accept-package-agreements --accept-source-agreements
         Write-Host ""
     }
 
     # install Notepad++
-    $notepadqq = Read-Host "Möchtest du Notepad++ installieren? (Y/N)"
+    Write-Host "Möchtest du " -NoNewline
+    Write-Host "'Notepad++' " -ForegroundColor Yellow -NoNewline
+    Write-Host "installieren? (Y/N)" -NoNewline
+    $notepadqq = Read-Host 
     if ($notepadqq -eq 'Y' -or $notepadqq -eq 'y') {   
-        Write-Host "Notepad++ wird installiert..."
-        winget install Notepad++.Notepad++ -h --accept-package-agreements --accept-source-agreements
+        Write-Host "'Notepad++' " -ForegroundColor Yellow -NoNewline
+        Write-Host "wird installiert..."
+        #winget install Notepad++.Notepad++ -h --accept-package-agreements --accept-source-agreements
         Write-Host ""
     }
 
     # install Obsidian
-    $obsidian = Read-Host "Möchtest du Obsidian installieren? (Y/N)"
+    Write-Host "Möchtest du " -NoNewline
+    Write-Host "'Obsidian' " -ForegroundColor Yellow -NoNewline
+    Write-Host "installieren? (Y/N)" -NoNewline
+    $obsidian = Read-Host 
     if ($obsidian -eq 'Y' -or $obsidian -eq 'y') { 
-        Write-Host "Obsidian wird installiert..."
-        winget install Obsidian.Obsidian -h --accept-package-agreements --accept-source-agreements
+        Write-Host "'Obsidian' " -ForegroundColor Yellow -NoNewline
+        Write-Host "wird installiert..."
+        #winget install Obsidian.Obsidian -h --accept-package-agreements --accept-source-agreements
         Write-Host ""
     }
 
     # install Anki
-    $anki = Read-Host "Möchtest du Anki installieren? (Y/N)"
+    Write-Host "Möchtest du " -NoNewline
+    Write-Host "'Anki' " -ForegroundColor Yellow -NoNewline
+    Write-Host "installieren? (Y/N)" -NoNewline
+    $anki = Read-Host
     if ($anki -eq 'Y' -or $anki -eq 'y') { 
-        Write-Host "Anki wird installiert..."
-        winget install Anki.Anki -h --accept-package-agreements --accept-source-agreements
+        Write-Host "'Anki' " -ForegroundColor Yellow -NoNewline
+        Write-Host "wird installiert..."
+        #winget install Anki.Anki -h --accept-package-agreements --accept-source-agreements
         Write-Host ""
     } else {
         Write-Host ""
@@ -163,10 +219,14 @@ if ($input1 -eq 'Y' -or $input1 -eq 'y') {
     Start-Sleep -Seconds 0.5
 
     # install Discord
-    $discord = Read-Host "Möchtest du Discord installieren? (Y/N)"
+    Write-Host "Möchtest du " -NoNewline
+    Write-Host "'Discord' " -ForegroundColor Yellow -NoNewline
+    Write-Host "installieren? (Y/N)" -NoNewline
+    $discord = Read-Host
     if ($discord -eq 'Y' -or $discord -eq 'y') { 
-        Write-Host "Discord wird installiert..."
-        winget install Discord.Discord -h --accept-package-agreements --accept-source-agreements
+        Write-Host "'Discord' " -ForegroundColor Yellow -NoNewline
+        Write-Host "wird installiert..."
+        #winget install Discord.Discord -h --accept-package-agreements --accept-source-agreements
         Write-Host ""
     } else {
         Write-Host ""
@@ -184,26 +244,38 @@ if ($input1 -eq 'Y' -or $input1 -eq 'y') {
     Start-Sleep -Seconds 0.5
 
     # install VLC media player
-    $vlc = Read-Host "Möchtest du VLC media player installieren? (Y/N)"
+    Write-Host "Möchtest du " -NoNewline
+    Write-Host "'VLC media player' " -ForegroundColor Yellow -NoNewline
+    Write-Host "installieren? (Y/N)" -NoNewline
+    $vlc = Read-Host
     if ($vlc -eq 'Y' -or $vlc -eq 'y') {
-        Write-Host "VLC media player wird installiert..."
-        winget install VideoLAN.VLC -h --accept-package-agreements --accept-source-agreements
+        Write-Host "'VLC media player' " -ForegroundColor Yellow -NoNewline
+        Write-Host "wird installiert..."
+        #winget install VideoLAN.VLC -h --accept-package-agreements --accept-source-agreements
         Write-Host ""
     }
 
     # install iTunes
-    $itunes = Read-Host "Möchtest du iTunes installieren? (Y/N)"
+    Write-Host "Möchtest du " -NoNewline
+    Write-Host "'iTunes' " -ForegroundColor Yellow -NoNewline
+    Write-Host "installieren? (Y/N)" -NoNewline
+    $itunes = Read-Host
     if ($itunes -eq 'Y' -or $itunes -eq 'y') {
-        Write-Host "iTunes wird installiert..."
-        winget install Apple.iTunes -h --accept-package-agreements --accept-source-agreements
+        Write-Host "'iTunes' " -ForegroundColor Yellow -NoNewline
+        Write-Host "wird installiert..."
+        #winget install Apple.iTunes -h --accept-package-agreements --accept-source-agreements
         Write-Host ""
     }
 
     # install OBS Studio
-    $obs = Read-Host "Möchtest du OBS Studio installieren? (Y/N)"
+    Write-Host "Möchtest du " -NoNewline
+    Write-Host "'OBS Studio' " -ForegroundColor Yellow -NoNewline
+    Write-Host "installieren? (Y/N)" -NoNewline
+    $obs = Read-Host
     if ($obs -eq 'Y' -or $obs -eq 'y') {
-        Write-Host "OBS Studio wird installiert..."
-        winget install OBSProject.OBSStudio -h --accept-package-agreements --accept-source-agreements
+        Write-Host "'OBS Studio' " -ForegroundColor Yellow -NoNewline
+        Write-Host "wird installiert..."
+        #winget install OBSProject.OBSStudio -h --accept-package-agreements --accept-source-agreements
         Write-Host ""
     } else {
         Write-Host ""
@@ -222,34 +294,50 @@ if ($input1 -eq 'Y' -or $input1 -eq 'y') {
     Start-Sleep -Seconds 0.5
     
     # install 7zip
-    $7zip = Read-Host "Möchtest du 7zip installieren? (Y/N)"
+    Write-Host "Möchtest du " -NoNewline
+    Write-Host "'7zip' " -ForegroundColor Yellow -NoNewline
+    Write-Host "installieren? (Y/N)" -NoNewline
+    $7zip = Read-Host
     if ($7zip -eq 'Y' -or $7zip -eq 'y') {
-        Write-Host "7zip wird installiert..."
-        winget install 7zip.7zip -h --accept-package-agreements --accept-source-agreements
+        Write-Host "'7zip' " -ForegroundColor Yellow -NoNewline
+        Write-Host "wird installiert..."
+        #winget install 7zip.7zip -h --accept-package-agreements --accept-source-agreements
         Write-Host ""
     }
 
     # install TranslucentTB
-    $translucent = Read-Host "Möchtest du TranslucentTB installieren? (Y/N)"
+    Write-Host "Möchtest du " -NoNewline
+    Write-Host "'TranslucentTB' " -ForegroundColor Yellow -NoNewline
+    Write-Host "installieren? (Y/N)" -NoNewline
+    $translucent = Read-Host
     if ($translucent -eq 'Y' -or $translucent -eq 'y') {
-        Write-Host "TranslucentTB wird installiert..."
-        winget install CharlesMilette.TranslucentTB -h --accept-package-agreements --accept-source-agreements
+        Write-Host "'TranslucentTB' " -ForegroundColor Yellow -NoNewline
+        Write-Host "wird installiert..."
+        #winget install CharlesMilette.TranslucentTB -h --accept-package-agreements --accept-source-agreements
         Write-Host ""
     }
 
     # install Microsoft Powertoys
-    $powertoys = Read-Host "Möchtest du Microsoft Powertoys installieren? (Y/N)"
+    Write-Host "Möchtest du " -NoNewline
+    Write-Host "'Microsoft Powertoys' " -ForegroundColor Yellow -NoNewline
+    Write-Host "installieren? (Y/N)" -NoNewline
+    $powertoys = Read-Host 
     if ($powertoys -eq 'Y' -or $powertoys -eq 'y') {
-        Write-Host "Microsoft Powertoys wird installiert..."
-        winget install Microsoft.PowerToys -h --accept-package-agreements --accept-source-agreements
+        Write-Host "'Microsoft Powertoys' " -ForegroundColor Yellow -NoNewline
+        Write-Host "wird installiert..."
+        #winget install Microsoft.PowerToys -h --accept-package-agreements --accept-source-agreements
         Write-Host ""
     }
 
     # install QuickLook
-    $quicklook = Read-Host "Möchtest du QuickLook installieren? (Y/N)"
+    Write-Host "Möchtest du " -NoNewline
+    Write-Host "'QuickLook' " -ForegroundColor Yellow -NoNewline
+    Write-Host "installieren? (Y/N)" -NoNewline
+    $quicklook = Read-Host
     if ($quicklook -eq 'Y' -or $quicklook -eq 'y') {
-        Write-Host "QuickLook wird installiert..."
-        winget install QL-Win.QuickLook -h --accept-package-agreements --accept-source-agreements
+        Write-Host "'QuickLook' " -ForegroundColor Yellow -NoNewline
+        Write-Host "wird installiert..."
+        #winget install QL-Win.QuickLook -h --accept-package-agreements --accept-source-agreements
         Write-Host ""
     } else {
         Write-Host ""
